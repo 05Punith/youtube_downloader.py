d3e19a71d7a3011664bc3b943b96c6f693056861
Punith <86360759+05Punith@users.noreply.github.com>
2021-08-29 12:03:34 +0530

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,7 +1,7 @@
 import csv
 import smtplib
-
 from pytube import YouTube
+#Importing csv file 
 with open('Mailid.csv','r') as file:
     reader=csv.reader(file)
     for row in reader:
@@ -9,14 +9,16 @@ with open('Mailid.csv','r') as file:
         print(type(row[2]))
         if row[2].strip() !="urls":
             youtube_link=row[2]
+            #downloading youtube vedio 
             YouTube(youtube_link).streams.first().download()
+            #Sending mail notification message after downloading 
             msg= EmailMessage()
             msg['Subject']="Vedio Download result"
-            msg['From']="086punith@gmail.com"
+            msg['From']="####@gmail.com"
             msg['To']=row[1]
             msg.sent_content("Vedio downloaded succesfully")
-            mail_id=r"0865punith@gmail.com"
-            Passord=r"punith0865"
+            mail_id=r"####@gmail.com"
+            Password=r"####"
             with smtplib.SMTP_SSL('smtp.gmail.com',465) as server:
                 server.login(mail_id,Password)
                 server.sendmessage(msg)
